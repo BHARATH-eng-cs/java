6cc4281745e580362fcd33bba60e58a969f05b9a
BHARATH T <bharathcse1122@gmail.com>
2025-10-26 21:51:38 +0530

    Add files via upload

---
+++ swappingtwonumbers.java
@@ -0,0 +1,9 @@
+public class swappingtwonumbers {
+    public static void main(String[] args) {
+        int a = 24, b  = 35 ;
+        int  temp = a;
+        a = b;
+        b = temp;
+        System.out.println("after swapping a="+a+",b = "+b);
+    }
+}

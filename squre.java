e50f3b10e425e51c928d2f491780d6369eb15381
BHARATH T <bharathcse1122@gmail.com>
2025-10-26 22:04:29 +0530

    Add files via upload

---
+++ squre.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class squre {
+    public static void main(String[] args) {
+        Scanner sc=new Scanne(System.in);
+        System.out.println("enter size of squre");
+        int squre = sc.nextInt();
+        int total = squre*squre;
+        System.out.println("total size of squre" + total);
+
+    }
+}

8b84c4373f682a07133627d081c081cb960ad686
BHARATH-eng-cs <bharathcse1122@gmail.com>
2025-10-29 20:41:39 +0530

    variable program added

---
+++ Variable.java
@@ -0,0 +1,11 @@
+public class Variable {
+    public static void main(String[] args) {
+        int age = 4;
+         String name = "Maha";
+         double height = 150.01;
+         System.out.println("applicant age: "+age);
+         System.out.println("applicant name: "+name);
+         System.out.println("applicant  height: "+height);
+        
+    }
+}

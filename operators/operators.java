56a578fd1106504282eae855eb4cf1b1c854b257
BHARATH-eng-cs <bharathcse1122@gmail.com>
2025-10-31 11:01:19 +0530

    operator file added

---
+++ operators/operators.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class operators {
+    public static void main(String[] args) {
+
+    int a = 10;
+    int b = 12;
+
+    int sum = a + b;
+    int sub = a - b;
+    int multi = a * b;
+    int div = a / b;
+  System.out.println("addition: "+sum);
+  System.out.println("sburaction: "+sub);
+  System.out.println("multiplication: "+multi);
+  System.out.println("devition:"+div);
+    
+}
+}

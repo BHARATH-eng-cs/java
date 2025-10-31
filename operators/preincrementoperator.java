5177946fd7219bed3ed8a5f377af1f3a81167f50
BHARATH-eng-cs <bharathcse1122@gmail.com>
2025-10-31 12:16:08 +0530

    pre increment file added

---
+++ operators/preincrementoperator.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class preincrementoperator {
+ public static void main(String[] args) {
+    int a = 6;
+    int b = ++a;
+    System.out.println("pre increment operator");
+    System.out.println("a ="+a);
+    System.out.println("b ="+b);
+ }   
+}

40ef3dd7d5bafcc0d3c9cfb7004640fa62941d79
BHARATH-eng-cs <bharathcse1122@gmail.com>
2025-10-31 12:10:42 +0530

    post increment file added

---
+++ operators/incrementoperator.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class incrementoperator {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = a++;
+    
+        System.out.println("pre increment");
+        System.out.println("a= "+a);
+         System.out.println("b= "+b);
+       
+        
+    }
+}

ccaca1ec0fef56d8b82bf8946c3eaaee5d3ca2bb
BHARATH-eng-cs <bharathcse1122@gmail.com>
2025-10-31 12:39:43 +0530

    All  relational file added

---
+++ operators/relationaloperator.java
@@ -0,0 +1,46 @@
+package operators;
+
+public class relationaloperator {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 11; 
+        int num3 =11;
+
+        System.out.println("num1 == num2 :"+(num1 == num2 ));
+        System.out.println("num1 == num3 :"+(num1 == num3 ));
+        System.out.println("num2 == num3 :"+(num2 == num3 ));
+
+System.out.println("------------------------------------------------------------------------------------------------------------");
+
+        System.out.println("num1 > num2 :"+(num1 > num2 ));
+        System.out.println("num1 > num3 :"+(num1 > num3 ));
+         System.out.println("num2 > num3 :"+(num2 > num3 ));
+
+System.out.println("------------------------------------------------------------------------------------------------------------");
+
+        System.out.println("num1 < num2 :"+(num1 < num2 ));
+        System.out.println("num1 < num3 :"+(num1 < num3 ));
+        System.out.println("num2 < num3 :"+(num2 < num3 ));
+
+
+System.out.println("------------------------------------------------------------------------------------------------------------");
+
+        System.out.println("num1 >= num2 :"+(num1 >= num2 ));
+        System.out.println("num1 >= num3 :"+(num1 >= num3));
+         System.out.println("num2 >= num3 :"+(num2 >= num3));
+    
+System.out.println("------------------------------------------------------------------------------------------------------------");
+
+        System.out.println("num1 <= num2 :"+(num1 <= num2 ));
+        System.out.println("num1 <= num3 :"+(num1 <= num3 ));
+        System.out.println("num2 <= num3 :"+(num2 <= num3 ));
+
+System.out.println("------------------------------------------------------------------------------------------------------------");
+
+        System.out.println("num1 != num2 :"+(num1 != num2 ));
+        System.out.println("num1 != num3 :"+(num1 != num3 ));
+         System.out.println("num2 != num3 :"+(num2 != num3 ));
+
+System.out.println("------------------------------------------------------------------------------------------------------------");
+    }
+}

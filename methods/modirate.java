248e566f41938bbee4955a371d43177acf90fe3e
BHARATH-eng-cs <bharathcse1122@gmail.com>
2025-11-01 21:46:29 +0530

    modirate file added

---
+++ methods/modirate.java
@@ -0,0 +1,12 @@
+package methods;
+
+public class modirate {
+    static void add(int a ,int b ){
+       int sum= a + b;
+       System.out.println("sum: "+sum);
+    }
+    public static void main(String[] args) {
+        add(20 , 10);
+        add(10 , 20);
+    }
+}

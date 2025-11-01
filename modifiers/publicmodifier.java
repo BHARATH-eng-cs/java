f4d7024c65da80186733b2b5b891fc86cc3a77c2
BHARATH-eng-cs <bharathcse1122@gmail.com>
2025-11-01 22:22:23 +0530

    public modifier code is added

---
+++ modifiers/publicmodifier.java
@@ -0,0 +1,11 @@
+package modifiers;
+
+public class publicmodifier {
+    static void bharath(){
+        System.out.println("vaanga vanakkam");
+    }
+    public static void main(String[] args) {
+        publicmodifier obj = new publicmodifier();
+        obj.bharath();
+    }
+}

e417898e883e9dde584d4b18af15cf577a38480e
BHARATH-eng-cs <bharathcse1122@gmail.com>
2025-11-02 13:20:48 +0530

    array file edded

---
+++ arrays/simple.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class simple {
+    public static void main(String[] args) {
+         int[] arr = { 12, 34, 4, 33};
+        int n = arr.length;
+        for (int i = 0; i < n; i++)
+        System.out.println(arr[i]+" ");
+    } 
+}
--- modifiers/class/Student.java
+++
@@ -1,9 +0,0 @@
-package modifiers.class;
-
-public class student {
-    public string name;
-
-    public void displayName(){
-        System.out.println("student Name: "+ name);
-    }
-    

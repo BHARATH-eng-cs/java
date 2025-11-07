2a9446492655d18c3ed03a5411ecc1706697d923
BHARATH-eng-cs <bharathcse1122@gmail.com>
2025-11-07 20:40:26 +0530

    new program added

--- type1/type2.java
+++ type1/type2.java
@@ -3,7 +3,7 @@ package type1;
 public class type2 {
     public static void main(String[] args) {
         for( int i = 1; i <=5; i++){
-            for(int j = 1; j <= i; j++){
+            for(int j = 1;j <= i; j++){
                 System.out.print(j+" ");
             }
             System.out.println();
